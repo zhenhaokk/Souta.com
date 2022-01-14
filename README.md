6f88ee3b40d9af74640d90e658b94f34b5baca04
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-01-14 16:30:22 +1100

    p1
    p1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Souta.com
- project1
+# Souta
---
+++ script.js
@@ -0,0 +1,19 @@
+ 
+
+ function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" red", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " red";
+  }
+  document.getElementById("myLink").click();
+
+
+ 
